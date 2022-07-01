5b19e29af9e5701bfd1ddea3aacf03ec46ab7cc9
Bodiseowei Tonweriyai <bmtonweriyai@gmail.com>
2022-07-01 05:47:33 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+/**
+ *  * main - Entry point
+ *
+ *  * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+
+	putchar(10);
+
+	return (0);
+
+}
