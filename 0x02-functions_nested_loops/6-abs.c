fe0e0ebdaa62a6c9422e94b5cee74e483d8b2faf
Bodiseowei Tonweriyai <bmtonweriyai@gmail.com>
2022-07-05 09:24:48 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value of n
+ * @n: The number to be computed.
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	return (n);
+}
