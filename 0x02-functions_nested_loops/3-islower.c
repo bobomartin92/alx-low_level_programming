37c992587bd31e8a5c3aeba2b4c83ce811dd1625
Bodiseowei Tonweriyai <bmtonweriyai@gmail.com>
2022-07-05 08:15:30 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks if c is lower case
+ * @c: The number to be checked.
+ *
+ * Return: 1 if lower else 0
+ */
+int _islower(int c)
+{
+	if (c > 96)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
