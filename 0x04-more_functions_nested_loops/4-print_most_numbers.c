e09160407404eab09bfee5fd7f4a1152486f100e
Bodiseowei Tonweriyai <bmtonweriyai@gmail.com>
2022-07-07 04:06:50 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  * print_most_numbers - Write a function that prints the numbers, from 0 to 9, followed by a new line.
+ */
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		if (i == 50 || i == 52)
+		{
+			i++;
+			continue;
+		}
+		_putchar(i);
+		i++;
+	}
+	_putchar(10);
+}
