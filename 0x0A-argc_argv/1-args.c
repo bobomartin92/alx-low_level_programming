0a359f18440e5640a7761e7f421b6da071e59fc4
Bodiseowei Tonweriyai <bmtonweriyai@gmail.com>
2022-07-22 04:09:06 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program thats prints its name, followed by a new line.
+ * @argc: stores number of command-line arguments passed by the
+ * user including the name of the program.
+ * @argv: is array of character pointers listing all the arguments.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argv)
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
